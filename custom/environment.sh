aea2511cd566ed5b08a96ffebe6e39955c26067d
Jesse Steinberg <jessesteinberg01@gmail.com>
2022-11-14 22:14:11 -0500

    custom environment changes

--- custom/environment.sh
+++ custom/environment.sh
@@ -1,8 +1,10 @@
+# NVD-API KEY -- nvd-search
+export NVD_KEY="6dff5dbd-bad8-4061-9ae2-5cad7010a93b"
+
 # Shell theme setup
 export PROMPT_THEME='bart'
 
-export JAVA_HOME="/usr/lib/jvm/java-1.11.0-openjdk-amd64"
-export PATH=$PATH:$JAVA_HOME/bin
+#export JAVA_HOME=`/usr/libexec/java_home -v 17`
 
 # Colored GCC warnings, errors
 export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
@@ -11,4 +13,6 @@ export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quo
 # Make less more friendly for non-text input files, see lesspipe(1)
 [ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
 
+# Add my scripts to path
+export PATH=$PATH:"/Users/jesse/scripts/"
 
