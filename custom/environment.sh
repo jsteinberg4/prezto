30cff4aa9d953aecd92c7c412d7d48a21f3c1f54
Jesse Steinberg <jessesteinberg01@gmail.com>
2023-03-26 23:17:44 -0400

    Remove api key + add nvim + add python3.11

--- custom/environment.sh
+++ custom/environment.sh
@@ -1,6 +1,3 @@
-# NVD-API KEY -- nvd-search
-export NVD_KEY="6dff5dbd-bad8-4061-9ae2-5cad7010a93b"
-
 # Shell theme setup
 export PROMPT_THEME='bart'
 
@@ -9,10 +6,8 @@ export PROMPT_THEME='bart'
 # Colored GCC warnings, errors
 export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
 
-
 # Make less more friendly for non-text input files, see lesspipe(1)
 [ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
 
 # Add my scripts to path
 export PATH=$PATH:"/Users/jesse/scripts/"
-
