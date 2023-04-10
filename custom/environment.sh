323e59a3f3ddc1ee97a2acf57879ad907d7935aa
jsteinberg <jesse.steinberg@recordedfuture.com>
2023-04-10 13:48:36 -0400

    Fix other typo

--- custom/environment.sh
+++ custom/environment.sh
@@ -10,6 +10,6 @@ export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quo
 [ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
 
 # Add my scripts to path
-if [ -x "$HOME/scripts/"]; then
+if [ -x "$HOME/scripts/" ]; then
   export PATH="$HOME/scripts/:${PATH}"
 fi
